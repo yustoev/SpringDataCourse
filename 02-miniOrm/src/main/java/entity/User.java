ddeca9bc3b0c168b9e5ccfebbfd0cc4a78e6fe6a
yustoev <76880702+yustoev@users.noreply.github.com>
2021-08-01 11:40:11 +0300

    User created

---
+++ 02-miniOrm/src/main/java/entity/User.java
@@ -0,0 +1,85 @@
+package entity;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private int age;
+    private String registrationDate;
+
+    public User() {
+    }
+
+    public User(String username, String password, int age, String registrationDate) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.registrationDate = registrationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", registrationDate='" + registrationDate + '\'' +
+                '}';
+    }
+}
