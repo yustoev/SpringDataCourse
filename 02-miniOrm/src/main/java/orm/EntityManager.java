63b3478177988232c47c840b0ed0c447651a7f3b
yustoev <76880702+yustoev@users.noreply.github.com>
2021-08-01 21:44:08 +0300

    doInsert completed

--- 02-miniOrm/src/main/java/orm/EntityManager.java
+++ 02-miniOrm/src/main/java/orm/EntityManager.java
@@ -1,13 +1,24 @@
 package orm;
 
+import orm.annotation.Column;
 import orm.annotation.Entity;
 import orm.annotation.Id;
 
 import java.lang.reflect.Field;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class EntityManager<E> implements DbContext<E>{
+    private static final String SELECT_STAR_FROM = "SELECT * FROM ";
+    private static final String INSERT_QUERY = "INSERT INTO %s (id, %s) VALUE (%s) ;";
+    private static final String UPDATE_QUERY = "UPDATE %s SET %s WHERE %s ;";
+    private static final String DELETE_QUERY = "DELETE FROM %s WHERE %s ;";
     private Connection connection;
 
     public EntityManager(Connection connection) {
@@ -15,7 +26,7 @@ public class EntityManager<E> implements DbContext<E>{
     }
 
     @Override
-    public boolean persist(E entity) throws IllegalAccessException {
+    public boolean persist(E entity) throws IllegalAccessException, SQLException {
         Field primary = getIdField(entity.getClass());
         primary.setAccessible(true);
         Object value = primary.get(entity);
@@ -50,9 +61,21 @@ public class EntityManager<E> implements DbContext<E>{
         return false;
     }
 
-    private boolean doInsert(E entity, Field primary) {
-        return false;
+    private boolean doInsert(E entity, Field primary) throws SQLException {
+        String tableName = getTableName(entity.getClass());
+        List<String> fieldNames = getFieldNames(entity);
+        List<String> fieldValues = getFieldValues(entity);
+        String insertQuery = String.format(INSERT_QUERY, tableName, fieldNames, fieldValues);
+        return executeQuary(insertQuery);
     }
+
+    private boolean executeQuary(String sql) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(sql);
+        return ps.execute();
+
+    }
+
+
     private Field getIdField(Class entity){
         return Arrays.stream(entity.getDeclaredFields())
                 .filter(field -> field.isAnnotationPresent(Id.class))
@@ -62,10 +85,40 @@ public class EntityManager<E> implements DbContext<E>{
 
     private String getTableName(Class<?> entity){
         Entity entityAnnotation = entity.getAnnotation(Entity.class);
-        if(entityAnnotation != null && entityAnnotation.name() != null && entityAnnotation.name().length() > 0){
+        if(entityAnnotation != null && entityAnnotation.name().length() > 0){
             return entityAnnotation.name();
         }else {
             return entity.getSimpleName();
         }
+    }
+    private List<String> getFieldValues(E entity) {
+         Function<Field, String> getFieldValue = field -> {
+             field.setAccessible(true);
+             try {
+                 Object value = field.get(entity);
+                return field.getType() == String.class || field.getType() == LocalDate.class
+                         ? String.format("'%s'", value.toString())
+                         : value.toString();
+             } catch (IllegalAccessException e) {
+                 e.printStackTrace();
+             }
+             return "";
+         };
+        return Arrays.stream(entity.getClass().getDeclaredFields())
+                .filter(f -> f.isAnnotationPresent(Column.class))
+                .map(getFieldValue)
+                .collect(Collectors.toList());
+    }
+
+
+
+    private List<String> getFieldNames(E entity) {
+       return Arrays.stream(entity.getClass().getDeclaredFields())
+                .filter(f -> f.isAnnotationPresent(Column.class))
+                .map(x -> {
+                    x.setAccessible(true);
+                    return x.getAnnotation(Column.class).name();
+                })
+        .collect(Collectors.toList());
     }
 }
