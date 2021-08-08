95557353022c6e6a7805c273f6f15e59d333bbf2
yustoev <76880702+yustoev@users.noreply.github.com>
2021-08-08 18:06:58 +0300

    Hibernate skeleton added

--- hibernateintro/src/main/java/Main.java
+++ hibernateintro/src/main/java/Main.java
@@ -1,5 +1,13 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("soft_uni");
+        EntityManager em = factory.createEntityManager();
+
 
     }
 }
